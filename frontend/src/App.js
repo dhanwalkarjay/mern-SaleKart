bfd3be73b5b1d91f75d55644c38776f9ba95fae3
jay <jaydhanwalkar123@gmail.com>
2023-10-12 20:15:14 +0530

    E-commerce-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Sales Kart</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
